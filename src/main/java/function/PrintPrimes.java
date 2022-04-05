98766cf8e61a4dde98439b90dc4c04a4a8e1cf9c
parkhojeong <parkhj062@gmail.com>
2022-04-05 17:37:23 +0900

    extract parameter, local variable

--- src/main/java/function/PrintPrimes.java
+++ src/main/java/function/PrintPrimes.java
@@ -12,10 +12,6 @@ public class PrintPrimes {
         private int columns = 4;
         private int ordmax = 30;
         private int[] primes;
-        private int pagenumber;
-        private int pageoffset;
-        private int rowoffset;
-        private int column;
         private int candidate;
         private int primeIndex;
         private boolean possiblyPrime;
@@ -51,18 +47,19 @@ public class PrintPrimes {
                 primeIndex = primeIndex + 1;
                 primes[primeIndex] = candidate;
             }
-            printPrimes(numberOfPrimes, linesPerPage);
+            printPrimes(numberOfPrimes, linesPerPage, columns, primes);
         }
 
-        private void printPrimes(int numberOfPrimes, int linesPerPage) {
-            pagenumber = 1;
-            pageoffset = 1;
+        private void printPrimes(int numberOfPrimes, int linesPerPage, int columns, int[] primes) {
+            int pagenumber = 1;
+            int pageoffset = 1;
+
             while (pageoffset <= numberOfPrimes) {
                 System.out.println("The First " + numberOfPrimes +
                         " Prime Numbers --- Page " + pagenumber);
                 System.out.println("");
-                for (rowoffset = pageoffset; rowoffset < pageoffset + linesPerPage; rowoffset++) {
-                    for (column = 0; column < columns; column++)
+                for (int rowoffset = pageoffset; rowoffset < pageoffset + linesPerPage; rowoffset++) {
+                    for (int column = 0; column < columns; column++)
                         if (rowoffset + column * linesPerPage <= numberOfPrimes)
                             System.out.format("%10d", primes[rowoffset + column * linesPerPage]);
                     System.out.println("");
